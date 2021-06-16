806f5e95c0489f80f558bc012ec0f1416797b091
Daisuke Hasegawa <daisukehasegawa@daisukenoair.lan>
2021-06-16 22:23:53 +0900

    目次のリンクを追加 #56

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,12 @@ module.exports = {
       options: {
         gatsbyRemarkPlugins: [
           {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false,
+            },
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
